0b7fd2c98b184057b03b4ac935858fba1b06e098
Kiser <37519411+NCKiser@users.noreply.github.com>
2023-03-02 12:25:15 -0600

    2 buttons

--- config/boards/arm/twoli/custom_matrix_status.c
+++ config/boards/arm/twoli/custom_matrix_status.c
@@ -16,16 +16,22 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-static lv_style_t style_btn;
+static lv_style_t style_btn;000000
 
 int custom_widget_matrix_status_init(struct custom_widget_matrix_status *widget, lv_obj_t *parent) {
-    widget->obj = lv_btn_create(parent, NULL);
+    widget->obj = lv_obj_create(parent, NULL);
 
-    lv_obj_set_size(widget->obj, 30, 12); 
+    lv_obj_set_size(widget->obj, 48, 64); 
 
     lv_obj_align(widget->obj, NULL, LV_ALIGN_IN_RIGHT_MID, 0, 0);
 
-    //lv_obj_set_style_color(widget->obj, LV_STYLE_BORDER_COLOR, LV_COLOR_FG)
+    lv_obj_t* btn1 = lv_btn_create(widget->obj, NULL);
+    lv_obj_set_size(btn1, 16, 16); 
+    lv_obj_align(btn1, NULL, LV_ALIGN_IN_TOP_MID, 0, 0);
+
+    lv_obj_t* btn2 = lv_btn_create(widget->obj, NULL);
+    lv_obj_set_size(btn2, 16, 16); 
+    lv_obj_align(btn2, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
 
