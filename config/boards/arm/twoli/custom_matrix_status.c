e290bcaf5d5210fa09857efb27113903df2990fe
Kiser <37519411+NCKiser@users.noreply.github.com>
2023-03-02 07:25:29 -0600

    add matrix kconfigs, and beginnings of widget code

--- config/boards/arm/twoli/custom_matrix_status.c
+++ config/boards/arm/twoli/custom_matrix_status.c
@@ -1,1 +1,29 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <kernel.h>
+
+#include <logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#include <zmk/display.h>
 #include "custom_matrix_status.h"
+#include <zmk/event_manager.h>
+#include <zmk/events/position_state_changed.h>
+
+static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
+
+int custom_widget_matrix_status_init(struct custom_widget_matrix_status *widget, lv_obj_t *parent) {
+    widget->obj = lv_label_create(parent, NULL);
+    
+    lv_obj_set_size(widget->obj, 43, 64);
+    lv_label_set_text(widget->obj, "test");
+    lv_obj_align(widget->obj, NULL, LV_ALIGN_IN_RIGHT_MID, 0, 0);
+
+    sys_slist_append(&widgets, &widget->node);
+
+    return 0;
+}
