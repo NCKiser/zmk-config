2364e82f6437304f2d5c7cea0615882f7b884f34
Kiser <37519411+NCKiser@users.noreply.github.com>
2023-03-02 12:56:57 -0600

    agh

--- config/boards/arm/twoli/custom_matrix_status.c
+++ config/boards/arm/twoli/custom_matrix_status.c
@@ -26,12 +26,11 @@ int custom_widget_matrix_status_init(struct custom_widget_matrix_status *widget,
     lv_obj_t* top_half = lv_obj_create(widget->obj, NULL);
     lv_obj_set_size(top_half, 48, 32); 
     lv_obj_align(top_half, NULL, LV_ALIGN_IN_TOP_MID, 0, 0);
-    lv_obj_set_style_local_border_color(top_half, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, BG_COLOR)
+    lv_obj_set_style_local_border_color(top_half, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_THEME_DEFAULT_COLOR_PRIMARY)
 
     lv_obj_t* bottom_half = lv_obj_create(widget->obj, NULL);
     lv_obj_set_size(bottom_half, 48, 32); 
     lv_obj_align(bottom_half, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
-    lv_obj_add_style(bottom_half, &style_obj);
 
     sys_slist_append(&widgets, &widget->node);
 
