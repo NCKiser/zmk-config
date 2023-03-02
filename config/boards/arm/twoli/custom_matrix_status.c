781d333202def2c27fe60d5f19658dce9093b192
Kiser <37519411+NCKiser@users.noreply.github.com>
2023-03-02 11:16:58 -0600

    I have no clue what im doing

--- config/boards/arm/twoli/custom_matrix_status.c
+++ config/boards/arm/twoli/custom_matrix_status.c
@@ -14,28 +14,25 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/event_manager.h>
 #include <zmk/events/position_state_changed.h>
 
-static lv_style_t button_style;
-
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
 int custom_widget_matrix_status_init(struct custom_widget_matrix_status *widget, lv_obj_t *parent) {
     widget->obj = lv_btn_create(parent, NULL);
 
-    lv_style_set_bg_color(&button_style, LV_STATE_DEFAULT, LV_COLOR_BLACK);
-    lv_style_set_bg_color(&button_style, LV_STATE_PRESSED, LV_COLOR_WHITE);
-    lv_style_set_border_color(&button_style, LV_STATE_DEFAULT, LV_COLOR_WHITE);
-    lv_style_set_border_width(&button_style, LV_STATE_DEFAULT, 2);
-    lv_style_set_border_color(&button_style, LV_STATE_PRESSED, LV_COLOR_WHITE);
-    lv_style_set_border_width(&button_style, LV_STATE_PRESSED, 2);
+    theme = lv_theme_mono_init(0, NULL);
+    lv_theme_set_current(theme);
 
-    lv_obj_set_size(widget->obj, 48, 64);
-    lv_obj_add_style(widget->obj, LV_OBJ_PART_MAIN, &button_style);
+    // set the size of the button
+    lv_obj_set_size(widget->obj, 30, 12); 
 
-    lv_btn_set_checkable(widget->obj, true);
-    lv_btn_set_state(widget->obj, LV_BTN_STATE_RELEASED);
+    // set the text for the button label
+    lv_obj_t* label = lv_label_create(btn, NULL);
+    lv_label_set_text(label, "->");
 
-    lv_label_set_text(lv_label_create(widget->obj, NULL), "btn");
+    lv_obj_add_style(widget->obj, LV_OBJ_PART_MAIN, &theme);
+
     lv_obj_align(widget->obj, NULL, LV_ALIGN_IN_RIGHT_MID, 0, 0);
+
     sys_slist_append(&widgets, &widget->node);
 
     return 0;
